ddf19da7c8844237a8228f1ca78c155c44cb170c
jawahar-singamsetty <srjawahar1999@gmail.com>
2026-02-24 14:25:20 +0530

    Chat creation and deletion

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
-from routers import users, projects, files
+from routers import users, projects, files, chats
 
 
 # Load environment variables
@@ -30,7 +30,7 @@ app.add_middleware(
 app.include_router(users.router)
 app.include_router(projects.router)
 app.include_router(files.router)
-
+app.include_router(chats.router)
 
 # Health check endpoints
 @app.get("/")
